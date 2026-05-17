3d744fa9c904e2a79826574766b5dd9ec1186170
itsikh <itsik.harel@gmail.com>
2026-05-17 21:05:59 +0300

    chore: release v0.0.27

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "io.itsikh.finnencer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 26
-        versionName = "0.0.26"
+        versionCode = 27
+        versionName = "0.0.27"
     }
 
     signingConfigs {
