c2b4aa001a92b0408c16e2f89d8fca1e9bebd680
itsikh <itsik.harel@gmail.com>
2026-05-18 22:46:35 +0300

    chore: release v0.0.51

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "io.itsikh.finnencer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 50
-        versionName = "0.0.50"
+        versionCode = 51
+        versionName = "0.0.51"
     }
 
     signingConfigs {
