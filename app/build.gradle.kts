e6ba538461edfc94cade2d72f082ee5a34e91318
itsikh <itsik.harel@gmail.com>
2026-05-24 19:15:35 +0300

    chore: release v0.0.91

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "io.itsikh.finnencer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 90
-        versionName = "0.0.90"
+        versionCode = 91
+        versionName = "0.0.91"
     }
 
     signingConfigs {
